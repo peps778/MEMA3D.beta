e053f4360655d764b1df846e2e7f8079f6c1d4f2
peps778 <77629942+peps778@users.noreply.github.com>
2023-06-10 20:10:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+    function toggleMenu() {
+    var menu = document.getElementById('navMenu');
+    menu.classList.toggle('show-nav');
+    
+    var menuIcon = document.querySelector('.menu');
+    menuIcon.classList.toggle('open');}
